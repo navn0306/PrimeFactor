e2466f1a50154556dd2bf5b8f126c2b479284c84
Navneet <navn0306@gmail.com>
2021-11-28 00:18:17 +0530

    Add: Added the welcome message for Prime factor program

---
+++ src/com/bridge/PrimeFactor/PrimeFactor.java
@@ -0,0 +1,7 @@
+package com.bridge.PrimeFactor;
+
+public class PrimeFactor {
+    public static void main(String[] args) {
+        System.out.println("\n Welcome, The program calculates the Prime Factor or a given number");
+    }
+}
